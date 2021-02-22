b98ea0551286780fee23de4a383eb60fa13f640b
Moira <moira.walker@live.nl>
2021-02-22 10:54:29 +0100

    deleted comments

--- src/main/java/nl/thinkbox/demo/controller/ProjectController.java
+++ src/main/java/nl/thinkbox/demo/controller/ProjectController.java
@@ -18,35 +18,30 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
-//    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @GetMapping(value = "/projects")
     public ResponseEntity<Object> getAllProjects() {
         List<Project> projects = projectService.getAllProjects();
         return new ResponseEntity<>(projects, HttpStatus.OK);
     }
 
-//    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @GetMapping(value = "/projects/{id}")
     public ResponseEntity<Object> getProjectById(@PathVariable("id") long id) {
         Project project = projectService.getProjectById(id);
         return new ResponseEntity<>(project, HttpStatus.OK);
     }
 
-//    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @DeleteMapping(value = "/projects/{id}")
     public ResponseEntity<Object> deleteProject(@PathVariable("id") long id) {
         projectService.deleteProject(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-//    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @PostMapping(value = "/projects")
     public ResponseEntity<Object> saveProjects(@RequestBody Project project) {
         long newId = projectService.saveProject(project);
         return new ResponseEntity<>(newId, HttpStatus.CREATED);
     }
 
-//    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @PutMapping(value = "/projects/{id}")
     public ResponseEntity<Object> updateProject(@PathVariable("id") int id, @RequestBody Project project) {
         projectService.updateProject(id, project);
